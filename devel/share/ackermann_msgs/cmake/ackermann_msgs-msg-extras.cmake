8db9122662a496b1cac551f209ea27a01ef681ba
blake-gaines <blake@uconn.edu>
2023-03-02 13:51:18 -0500

    Remove another duplicated directory

--- devel/share/ackermann_msgs/cmake/ackermann_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ackermann_msgs_MESSAGE_FILES "/home/jiangwei3/catkin_ws/src/ackermann_msgs/msg/AckermannDrive.msg;/home/jiangwei3/catkin_ws/src/ackermann_msgs/msg/AckermannDriveStamped.msg")
-set(ackermann_msgs_SERVICE_FILES "")
--- devel/share/race/cmake/race-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(race_MSG_INCLUDE_DIRS "/home/jiangwei3/catkin_ws/src/race/msg")
-set(race_MSG_DEPENDENCIES sensor_msgs)
